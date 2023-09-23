6521234448a738efa9f3271a048dbd32842e6a87
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-09-23 20:47:40 +0530

    made the hard coded card, data driven card from a json

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,32 @@
 import './Card.css';
 
+
+
 const Card = () => {
+    
+    const AI101 = {
+        "creator":"Tony stark",
+        "image" :"https://upload.wikimedia.org/wikipedia/en/f/f2/Robert_Downey_Jr._as_Tony_Stark_in_Avengers_Infinity_War.jpg" ,
+        "title":"Lets create an AI with Tony stark, the iron man",
+        "country": "USA",
+        "noOfRatings": 9,
+        "rating": 5.0,
+        "price": 69
+    }
+
+
      return(
         <div className="card">
-            <img className="img" src="https://upload.wikimedia.org/wikipedia/en/f/f2/Robert_Downey_Jr._as_Tony_Stark_in_Avengers_Infinity_War.jpg" alt ="tony stark iron man"/>
+            <img className="img" src={AI101.image} />
                 <div>
                     <span className="star" > * </span>
-                    <span className='rating'>5.0  </span>
-                    <span className="noOfRatings"> (6)  | </span>
-                    <span className='country'> USA </span>
+                    <span className='rating'> {AI101.rating}  </span>
+                    <span className="noOfRatings"> ({AI101.noOfRatings})  | </span>
+                    <span className='country'> {AI101.country} </span>
                     </div>
-                    <p className='courseName'>Lets create an AI with Tony Stark, the iron man</p>
-                    <p className='price' ><strong>from 69&#36;</strong> / person</p>
+                    <p className='courseName'> {AI101.title}</p>
+                    <p className='price' ><strong>from {AI101.price}&#36;</strong> / person</p>
                 
-                
-
-           
 
         </div>
      ) 

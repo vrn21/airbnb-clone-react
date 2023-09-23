fea35d8243ef228ed8654f9ee03f021339d66a60
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-09-24 01:44:24 +0530

    destructered props

--- src/components/CatCard.jsx
+++ src/components/CatCard.jsx
@@ -2,25 +2,25 @@ import "./CatCard.css"
 import {CatData} from '../assets/TempData.js'
 
 
-const TempCard = (props) =>{
+const CatCard = ({pfp,username,phoneNumber,mailID}) =>{
     
 
 
     return(
         <div className="cardT">
-            <img src={props.pfp} className="pfp" />
-            <p className="username"><strong>{props.username}</strong></p>
+            <img src={pfp} className="pfp" />
+            <p className="username"><strong>{username}</strong></p>
                 <div className="phone-details">
                     <img className="phone-logo" src="https://i.pinimg.com/originals/69/27/aa/6927aab364de747a87f59686e830c4bf.png" alt="phone-symbol" />
-                    <span className="phoneNo">{props.phoneNumber}</span>
+                    <span className="phoneNo">{phoneNumber}</span>
                 </div>
                 <div className="mail-details">
                         <img className="mail-logo" src="https://static.vecteezy.com/system/resources/previews/020/009/601/original/email-and-mail-icon-black-free-png.png" alt="mail-symbol" />
-                    <span className="mailID">{props.mailID}</span>
+                    <span className="mailID">{mailID}</span>
                 </div>
         </div>
     )
     
 }
 
-export default TempCard;
+export default CatCard;

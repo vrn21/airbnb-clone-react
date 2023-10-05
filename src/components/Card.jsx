0c776d2b5ba40682b54ccf1ec5348b7d797378f7
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-10-05 08:32:05 +0530

    Issues fixedd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import SingleCard from './SingleCard';
 
 const Card = () => {
     const eachCard = Data.map(eachElement => (
-        <div className='cards-div'>
+        <div className="card-div" >
         <SingleCard
         key={eachElement.id}
         {...eachElement}
@@ -14,7 +14,7 @@ const Card = () => {
         </div>
     ));
 
-    return <div>{eachCard}</div>;
+    return <div className='cards-div'>{eachCard}</div>;
 };
 
 
